3020c994c5a58062a1833272047f4231fca2666a
wishrd <wish.rd@gmail.com>
2024-12-15 17:02:06 +0100

    feat(reactive-transfer-state): library for bring up reactivity to @astro-tools/transfer-state using nanostores

---
+++ apps/docs/src/content/docs/state-management/_examples/reactive-transfer-state/uuid-store.ts
@@ -0,0 +1,5 @@
+import { atom } from 'nanostores';
+
+import { withTransferState } from '@astro-tools/reactive-transfer-state';
+
+export const uuidStore = withTransferState('uuid', atom<string | null>(null));
---
+++ packages/reactive-transfer-state/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup';
+import { peerDependencies } from './package.json';
+
+export default defineConfig((options) => {
+	const dev = !!options.watch;
+	return {
+		entry: ["src/**/*.ts"],
+		format: ["esm"],
+		target: "node18",
+		bundle: true,
+		dts: true,
+		sourcemap: true,
+		clean: true,
+		splitting: true,
+		minify: !dev,
+		external: [...Object.keys(peerDependencies), '@astro-tools:transfer-state'],
+		tsconfig: "tsconfig.json"
+	};
+});
