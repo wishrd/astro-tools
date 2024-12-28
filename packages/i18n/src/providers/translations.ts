21ec85be751b78218b57ca8ccbc46b1abcf0a67e
wishrd <wish.rd@gmail.com>
2024-12-28 19:10:20 +0100

    fix(i18n): typing

--- packages/i18n/src/providers/translations.ts
+++ packages/i18n/src/providers/translations.ts
@@ -1,10 +1,10 @@
-import type { I18nTranslationsProvider } from '@astro-tools/i18n';
+import type { I18nTranslationsProvider } from '../core/models/i18n-translations-provider.ts';
 
 import { mapTranslations, type Translations } from '../adapters/json-to-translations.ts';
 
 export function translationsProviderFactory(locales: Record<string, () => Promise<Translations>>): I18nTranslationsProvider {
 
-  return async (locale) => {
+  return async (locale: string) => {
     const loader = locales[locale];
     if (!loader) {
       throw new Error(`Missing translations loader for ${locale}`);
