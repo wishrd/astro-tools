fdd76787a3ede5a01dd1af7d02fa4f41051f85fa
wishrd <wish.rd@gmail.com>
2025-02-02 19:28:54 +0100

    fix: keep async_storage

--- packages/transfer-state/src/state-storage.ts
+++ packages/transfer-state/src/state-storage.ts
@@ -1,4 +1,4 @@
-import { AsyncLocalStorage } from 'node:async_hooks';
+import { AsyncLocalStorage } from 'async_hooks';
 
 import type { ServerTransferState } from './models/server-transfer-state.ts';
 
