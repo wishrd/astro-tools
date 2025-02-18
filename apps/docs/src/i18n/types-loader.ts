2a12414c8995285395c7a953284476f8957b3fab
wishrd <wish.rd@gmail.com>
2025-02-18 20:01:16 +0100

    feat: add compatibility with Astro 5

--- apps/docs/src/i18n/types-loader.ts
+++ apps/docs/src/i18n/types-loader.ts
@@ -1,5 +1,12 @@
+import { fileURLToPath } from 'node:url';
+import { readFile } from 'node:fs/promises';
 import type { I18nIntegrationTypesLoader } from '@astro-tools/i18n';
+import { join, parse } from 'node:path';
 
 export function typesLoader(): I18nIntegrationTypesLoader {
-  return () => import('./translations/en-US.json').then(json => json.default);
+  return async () => {
+    const base = parse(fileURLToPath(import.meta.url)).dir;
+    const buffer = await readFile(join(base, 'translations', 'en-US.json'));
+    return JSON.parse(buffer.toString('utf-8'));
+  };
 };
