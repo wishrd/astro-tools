8f87137e485ff192b3a8d9aaeaa2896e5928e6b1
wishrd <wish.rd@gmail.com>
2025-03-08 12:09:42 +0100

    fixes and docs

--- packages/mdocs/src/models/config.ts
+++ packages/mdocs/src/models/config.ts
@@ -1,8 +1,7 @@
-import type { StarlightUserConfigWithPlugins } from '@astrojs/starlight/utils/plugins';
 import type { Transformer } from './transformer.js';
 
 export interface Config {
   pattern?: string | string[];
   transformers?: Transformer[];
-  starlight: StarlightUserConfigWithPlugins;
+  starlight: unknown;
 }
